9770d4e962c2de42c9dcd3d72259df12fd467eac
StormKing969 <wijesinghesaj@gmail.com>
2021-08-09 22:40:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello")
+city = input("Please enter the city you grew up in\n")
+pet = input("What is your pet's name?\n")
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
